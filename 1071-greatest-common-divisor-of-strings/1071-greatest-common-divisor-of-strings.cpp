f4cb1e720eddc3dce7d81120e00e8ae93592d917
Bisman Sapra <bismansapra07@gmail.com>
2026-05-30 13:28:39 +0530

    Time: 0 ms (100%), Space: 9.8 MB (32.49%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2 != str2+str1)
+        {
+        return "";
+        }
+        else
+        {
+            int len = gcd(str1.size(), str2.size());
+            return str2.substr(0,len);
+        }
+
+    
+    }
+};
