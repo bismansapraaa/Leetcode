0ce156c19af5007e44135c975c44ad9d7e365f06
Bisman Sapra <bismansapra07@gmail.com>
2026-07-02 09:20:07 +0530

    Time: 0 ms (100%), Space: 7.8 MB (18.31%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low = 0;
+        int high = num - 1 ;
+        if(num == 1)
+        {
+            return true;
+        }
+        while(low<=high)
+        {
+            long long  mid = low + (high - low) /2 ;
+            if(mid*mid == num )
+            {
+                return true;
+            }
+            else if(mid*mid > num)
+            {
+                high = mid -1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return false;  
+    }
+};
