9f414e9f312def9bf4cbcc8ec0ed99eb56eff933
Bisman Sapra <bismansapra07@gmail.com>
2026-07-19 08:52:18 +0530

    Time: 0 ms (100%), Space: 16.1 MB (67.45%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -4,13 +4,6 @@ public:
         
         int maxx = *max_element(nums.begin(), nums.end());
          int minn = *min_element(nums.begin(), nums.end());
-       /* for(int i =0; i<minn;i++ )
-        {
-            if(maxx%i ==0 && minn%i==0){
-                gcd =i;
-            }
-           
-        }*/
         return gcd(maxx,minn);
 
     }
