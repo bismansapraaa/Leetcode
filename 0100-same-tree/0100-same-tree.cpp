4ef553d57a6435cbe18199f034857cefa45bbb8c
Bisman Sapra <bismansapra07@gmail.com>
2026-07-19 09:13:35 +0530

    Time: 0 ms (100%), Space: 13 MB (13.2%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,11 +12,11 @@
 class Solution {
 public:
 bool isSameTree(TreeNode* p, TreeNode* q) {
-    if (p == nullptr && q == nullptr) 
+    if (p == NULL && q == NULL) 
     {
         return true;
     }
-    if (p == nullptr || q == nullptr) 
+    if (p == NULL || q == NULL) 
     {
         return false;
     }
