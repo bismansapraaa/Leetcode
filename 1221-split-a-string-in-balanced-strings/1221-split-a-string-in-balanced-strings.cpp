62b87a941ae1e9b0e567692577c6ade00d56c4a5
Bisman Sapra <bismansapra07@gmail.com>
2026-08-25 12:05:50 +0530

    Time: 0 ms (100%), Space: 8.4 MB (57.61%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int count = 0;
+        int balance = 0;
+        for(int i =0;i<s.size();i++)
+        {
+            if(s[i]== 'R')
+            {
+                count ++;
+            }
+            else if(s[i]== 'L')
+            {
+                count --;
+            }
+            if(count ==0)
+            {
+                balance ++;
+            }
+        }
+        return balance;
+    }
+};
